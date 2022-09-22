25c3f533488f983e2d60977e5bf784cd82fea01f
alvardkhchoyan <104993918+alvardkhchoyan@users.noreply.github.com>
2022-09-22 17:42:30 +0400

    Update sum.cpp

--- sep-19-2022/sum.cpp
+++ sep-19-2022/sum.cpp
@@ -22,12 +22,12 @@ int main() {
     MOV DWORD PTR[rbp - 8], 0       //int i = 0
     jmp loop
 loop2:
-    mov eax, DWORD PTR[rbp - 8]
-//    mov eax, DWORD PTR[rbp-20+rax*4]
+    mov rax, DWORD PTR[rbp - 8]
+    mov eax, DWORD PTR[rbp-20+rax*4]
     add DWORD PTR[rbp - 4], eax
-    add DWORD PTR[rbp - 8], 1
+    add rax, 1
 loop1:
-    cmp DWORD PTR[rbp - 8], 2
+    cmp rax, 2
     jle loop2
     mov eax, 0
     pop rbp
