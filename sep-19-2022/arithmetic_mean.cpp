c6a7f4f9fe002490699b328211a8280539fc28c3
alvardkhchoyan <104993918+alvardkhchoyan@users.noreply.github.com>
2022-09-22 17:40:42 +0400

    Update arithmetic_mean.cpp

--- sep-19-2022/arithmetic_mean.cpp
+++ sep-19-2022/arithmetic_mean.cpp
@@ -24,17 +24,6 @@ int main() {
    mov eax, DWORD PTR[rbp - 12]
    add eax, edx
    div eax, 3
-//////////////////////////////
-    dev-i  poxaren
-//       movsx   rdx, eax
-//        imul    rdx, rdx, 1431655766
-//        mov     rcx, rdx
-//        shr     rcx, 32
-//        cdq
-//        mov     eax, ecx
-//        sub     eax, edx
-//////////////////////////////////////////////////
-
    mov DWORD PTR[rbp - 16], eax
    mov eax, 0
    pop rbp
